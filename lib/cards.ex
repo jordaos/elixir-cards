fd3437d3685abea9e1a4e4f84d5897e76fb25194
Jordão Macedo <jordao.souza05@gmail.com>
2024-12-05 11:20:29 -0300

    Create deck and save into a file

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,51 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for `Cards`.
-  """
+  def create_deck do
+    values = [
+      "Ace",
+      "Two",
+      "Three",
+      "Four",
+      "Five",
+      "Six",
+      "Seven",
+      "Eight",
+      "Nine",
+      "Ten",
+      "Jack",
+      "Queen",
+      "King"
+    ]
 
-  @doc """
-  Hello world.
+    suits = ["Spades", "Clubs", "Hearts", "Diamounds"]
 
-  ## Examples
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
+  end
 
-      iex> Cards.hello()
-      :world
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
 
-  """
-  def hello do
-    :world
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    Enum.split(deck, hand_size)
+  end
+
+  def save(deck, filename) do
+    binary = :erlang.term_to_binary(deck)
+    File.write(filename, binary)
+  end
+  
+  def load(filename) do
+    {status, binary} = File.read(filename)
+    
+    case status do
+      :ok -> :erlang.binary_to_term(binary)
+      :error -> "File not found"
+    end
   end
 end
