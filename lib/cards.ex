0402c669c7c7da1005787474ac876cf8754f427e
Jordão Macedo <jordao.souza05@gmail.com>
2024-12-05 12:00:35 -0300

    Add documentation module

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,4 +1,11 @@
 defmodule Cards do
+  @moduledoc """
+    Provides methods for creating and handling a deck of cards
+    """
+  
+  @doc """
+    Returns a list of string containing the cards of a deck
+    """
   def create_deck do
     values = [
       "Ace",
@@ -31,21 +38,42 @@ defmodule Cards do
     Enum.member?(deck, card)
   end
 
+  @doc """
+    Divides a deck into a hand and the remainder of the deck.
+    The `hand_size` argument indicates how many cards should be in the hand.
+    
+    ## Examples
+    
+        iex> deck = Cards.create_deck
+        iex> {hand, deck} = Cards.deal(deck, 1)
+        iex> hand
+        ["Ace of Spades"]
+    """
   def deal(deck, hand_size) do
     Enum.split(deck, hand_size)
   end
 
   def save(deck, filename) do
     binary = :erlang.term_to_binary(deck)
-    File.write(filename, binary)
+    File.write("decks/#{filename}", binary)
   end
   
   def load(filename) do
-    {status, binary} = File.read(filename)
-    
-    case status do
-      :ok -> :erlang.binary_to_term(binary)
-      :error -> "File not found"
+    case File.read("decks/#{filename}") do
+      {:ok, binary} -> :erlang.binary_to_term(binary)
+      {:error, reason} -> "File not found: #{reason}"
     end
+  end
+  
+  def create_hand(hand_size) do
+    # Instead of
+    # deck = Cards.create_deck
+    # deck = Cards.shuffle(deck)
+    # hand = Cards.deal(deck, hand_size)
+    
+    # do
+    Cards.create_deck
+    |> Cards.shuffle # Automatically inject the return for the prev function into the first argument of this function
+    |> Cards.deal(hand_size) # The first param was automatically injected by the pipe |>
   end
 end
